b45b101b7ce5b9e368796c53263202d410d39ffc
Ash-art-arch <120031662+Ash-art-arch@users.noreply.github.com>
2024-01-26 19:31:18 +0530

    user register and login

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const userroute = require("./routes/user");
+const cookieParser = require("cookie-parser");
 
 const PORT = 4000;
 
@@ -10,8 +12,16 @@ const app = express({
 });
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(cookieParser());
+app.use("/",userroute);
 
 app.listen(PORT,()=>{
     console.log("Server running on port : ",PORT);
+})
+
+
+mongoose.connect("mongodb://localhost:27017/sticky_notesDB").then(()=>{
+    console.log("DB connected");
 })
